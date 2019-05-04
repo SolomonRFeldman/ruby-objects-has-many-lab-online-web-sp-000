aa9603e306351bde47d2ef53adf81e375bbbf748
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-04 00:19:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,8 +19,8 @@ class Author
   end
   
   def add_post_by_title(post_title)
-    post_ = Post.new(song_name)
-    post_.artist = self
+    post = Post.new(song_name)
+    post.artist = self
     @songs << post
   end
   
