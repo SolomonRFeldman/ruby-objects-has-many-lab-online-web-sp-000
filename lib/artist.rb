a0b61a335286b0e3afd2164a645b1fc4a69344dc
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-04 00:04:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,12 @@
 class Artist
   attr_accessor :name
   
+  @@artists
+  
   def initialize(name)
     @name = name
     @songs = []
+    @@artists << self
   end
   
   def songs
@@ -22,7 +25,11 @@ class Artist
   end
   
   def self.song_count
-    
+    song_count = 0
+    @@artists.each do |artist|
+      song_count += artist.songs.count
+    end
+    song_count
   end
   
 end
